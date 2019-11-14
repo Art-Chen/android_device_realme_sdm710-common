bf503a52710f10f2417b1e66bb1e3b3c0c1f627e
Art_Chen <chenxy0201@qq.com>
2019-11-14 23:39:56 +0800

    Revert "sdm710-common: releasetools: remove unused or unnecessary imports"
    This reverts commit b3bd4515772d151ef4c40596a3e8e03dbe925644.

--- releasetools.py
+++ releasetools.py
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import hashlib
+import common
 import re
 
 def FullOTA_InstallEnd(info):
