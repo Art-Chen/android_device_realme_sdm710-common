eabc82d742458e5ef2401cdb7ee8a7fa04204a43
Art_Chen <chenxy0201@qq.com>
2020-02-25 18:12:22 +0800

    sdm710-common: Update prop for Q blobs
    Signed-off-by: Art_Chen <chenxy0201@qq.com>

--- vendor_props.mk
+++ vendor_props.mk
@@ -43,10 +43,61 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.audio.offload.pstimeout.secs=3 \
     vendor.voice.path.for.pcm.voip=true
 
+# Audio Dymatic Prop
+    vendor.audio.apptype.multirec.enabled=false \
+    vendor.audio.dolby.ds2.enabled=false \
+    vendor.audio.dolby.ds2.hardbypass=false \
+    vendor.audio.feature.a2dp_offload.enable=true \
+    vendor.audio.feature.afe_proxy.enable=true \
+    vendor.audio.feature.anc_headset.enable=true \
+    vendor.audio.feature.audiozoom.enable=false \
+    vendor.audio.feature.battery_listener.enable=false \
+    vendor.audio.feature.compr_cap.enable=false \
+    vendor.audio.feature.compr_voip.enable=false \
+    vendor.audio.feature.compress_in.enable=false \
+    vendor.audio.feature.compress_meta_data.enable=true \
+    vendor.audio.feature.concurrent_capture.enable=false \
+    vendor.audio.feature.custom_stereo.enable=true \
+    vendor.audio.feature.deepbuffer_as_primary.enable=false \
+    vendor.audio.feature.display_port.enable=true \
+    vendor.audio.feature.dsm_feedback.enable=false \
+    vendor.audio.feature.dynamic_ecns.enable=false \
+    vendor.audio.feature.ext_hw_plugin.enable=false \
+    vendor.audio.feature.external_dsp.enable=false \
+    vendor.audio.feature.external_speaker.enable=false \
+    vendor.audio.feature.external_speaker_tfa.enable=false \
+    vendor.audio.feature.fluence.enable=true \
+    vendor.audio.feature.fm.enable=true \
+    vendor.audio.feature.hdmi_edid.enable=true \
+    vendor.audio.feature.hdmi_passthrough.enable=true \
+    vendor.audio.feature.hfp.enable=true \
+    vendor.audio.feature.hifi_audio.enable=true \
+    vendor.audio.feature.hwdep_cal.enable=false \
+    vendor.audio.feature.incall_music.enable=false \
+    vendor.audio.feature.keep_alive.enable=false \
+    vendor.audio.feature.kpi_optimize.enable=true \
+    vendor.audio.feature.maxx_audio.enable=false \
+    vendor.audio.feature.multi_voice_session.enable=true \
+    vendor.audio.feature.ras.enable=true \
+    vendor.audio.feature.record_play_concurency.enable=false \
+    vendor.audio.feature.snd_mon.enable=true \
+    vendor.audio.feature.spkr_prot.enable=true \
+    vendor.audio.feature.src_trkn.enable=true \
+    vendor.audio.feature.ssrec.enable=true \
+    vendor.audio.feature.usb_offload.enable=true \
+    vendor.audio.feature.usb_offload_burst_mode.enable=false \
+    vendor.audio.feature.usb_offload_sidetone_volume.enable=false \
+    vendor.audio.feature.vbat.enable=true \
+    vendor.audio.feature.wsa.enable=false \
+    vendor.audio.volume.headset.gain.depcal=true \
+    vendor.audio_hal.period_size=192
+    
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.bt.a2dp_offload_cap=sbc-aptx-aptxhd-aac-ldac \
-    vendor.qcom.bluetooth.soc=cherokee
+    vendor.qcom.bluetooth.soc=cherokee \
+    ro.af.client_heap_size_kbyte=7168 \
+    ro.bluetooth.a2dp_offload.supported=true
 
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -100,7 +151,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=libqti-perfd-client.so \
     ro.vendor.at_library=libqti-at.so \
-    persist.vendor.qti.games.gt.prof=1
+    persist.vendor.qti.games.gt.prof=1 \
+    vendor.power.pasr.enabled=true
 
 # Qualcomm System Daemon
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -113,5 +165,25 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.radio.custom_ecc=1 \
     persist.vendor.radio.rat_on=combine \
     persist.vendor.radio.sib16_support=1 \
-    rild.libpath=/vendor/lib64/libril-qc-hal-qmi.so
-
+    rild.libpath=/vendor/lib64/libril-qc-hal-qmi.so \
+    persist.vendor.radio.procedure_bytes=SKIP
+    
+    
+# Dirac
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.dirac.acs.controller=gef \
+    persist.dirac.config=64 \
+    persist.dirac.gef.exs.appt=0x00011130 \
+    persist.dirac.gef.exs.did=29,49 \
+    persist.dirac.gef.exs.mid=0x10012DE3 \
+    persist.dirac.gef.ext.appt=0x00011130,0x00011134,0x00011136 \
+    persist.dirac.gef.ext.did=10,20,29,49 \
+    persist.dirac.gef.ext.mid=0x10012DE1 \
+    persist.dirac.gef.ins.appt=0x00011130 \
+    persist.dirac.gef.ins.did=19,134,150 \
+    persist.dirac.gef.ins.mid=0x10012DE2 \
+    persist.dirac.gef.int.appt=0x00011130,0x00011134,0x00011136 \
+    persist.dirac.gef.int.did=15,19,134,150 \
+    persist.dirac.gef.int.mid=0x10012DE0 \
+    persist.dirac.path=/vendor/etc/dirac/ \
+    persist.dirac.qem.oppo.syss=true
