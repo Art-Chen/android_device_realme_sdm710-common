35b3533a6aeedf6a7cecb6ea4b2f7832b60ec508
Art_Chen <chenxy0201@qq.com>
2020-03-02 23:30:38 +0800

    sdm710-common: Add support for Alipay Fingerprint Payment

--- sdm710.mk
+++ sdm710.mk
@@ -132,6 +132,13 @@ PRODUCT_COPY_FILES += \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# AliPay Fingerprint Pay
+PRODUCT_PACKAGES += \
+    ifaamanager
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/ifaamanager/etc/permissions/org.ifaa.android.manager.permissions.xml:/$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.ifaa.android.manager.permissions.xml
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
