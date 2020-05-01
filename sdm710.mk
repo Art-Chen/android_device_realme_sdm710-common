36b2bb6c16ebdb444831646c09f91f789a2a9f5a
Art_Chen <chenxy0201@qq.com>
2020-05-01 21:51:21 +0800

    sdm710-common: Let's based on Realme UI

--- sdm710.mk
+++ sdm710.mk
@@ -150,7 +150,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider@2.4-service_64 \
     libxml2 \
     Snap
 
@@ -441,7 +441,9 @@ PRODUCT_PACKAGES += \
     libwpa_client \
     libwifi-hal-qcom \
     vendor.qti.hardware.wifi.hostapd@1.0.vendor \
+    vendor.qti.hardware.wifi.hostapd@1.1.vendor \
     vendor.qti.hardware.wifi.supplicant@2.0.vendor \
+    vendor.qti.hardware.wifi.supplicant@2.1.vendor \
     wpa_supplicant \
     wpa_supplicant.conf
 
